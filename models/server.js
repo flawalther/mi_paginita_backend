8822e87a0c3fc9a1d42f9dfcbf0d24a547dda444
Flavia Walther <flamwalther@gmail.com>
2023-10-08 10:47:42 -0300

    primer commit crear servidor local y subo mi pag

---
+++ models/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = 8080;
+
+        
+        this.middlewares();
+
+    }
+
+    middlewares(){
+        this.app.use(express.static("public"));
+    }
+
+    routes(){
+
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log("Server online en puerto:", this.port);
+        })
+    }
+
+}
+
+module.exports = Server;
